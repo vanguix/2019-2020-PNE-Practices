5fcc88ecf67068ee0ec6c37bf985debfda96b327
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-02-04 15:16:45 +0100

    Sesion 2 testing

--- Hello/hello.py
+++ Hello/hello.py
@@ -2,4 +2,6 @@ print("This is my first python program in pycharm")
 print("hello")
 print("these are more and more test")
 
-#this is a comment
+#exercise S1.1
+print("Another print added!")
+print("just testing...")
