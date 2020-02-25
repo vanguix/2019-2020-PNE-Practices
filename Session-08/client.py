c395b2b93d0fbd9565f41f377b0ca65c920235ca
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-02-25 16:46:52 +0100

    session 8

---
+++ Session-08/client.py
@@ -0,0 +1,27 @@
+import socket
+
+# SERVER IP, PORT
+# Write here the correct parameter for connecting to the
+# Teacher's server
+PORT = 8080
+IP = "212.128.253.128"
+
+
+# First, create the socket
+# We will always use this parameters: AF_INET y SOCK_STREAM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
+
+# Send data. No strings can be send, only bytes
+# It necesary to encode the string into bytes
+s.send(str.encode("HELLO FROM THE CLIENT!!!"))
+
+# Receive data from the server
+msg = s.recv(2048)
+print("MESSAGE FROM THE SERVER:\n")
+print(msg.decode("utf-8"))
+
+# Closing the socket
+s.close()
