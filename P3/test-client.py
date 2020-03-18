94101efd95a5de9b3a94d4b8b4dd279398101006
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-03-18 18:39:14 +0100

    Practise 4 final

--- P3/test-client.py
+++ P3/test-client.py
@@ -6,10 +6,31 @@ PORT = 8083
 
 # -- Create a client object
 c = Client(IP, PORT)
+seq0 = "ACT"
+print("-----| Practice 3, Exercise 7 |-----")
 
+print("Testing PING...")
 print(c.talk("PING"))
+
+print("Testing GET...")
+print(c.talk("GET 0"))
+print(c.talk("GET 1"))
 print(c.talk("GET 2"))
-print(c.talk("INFO AGTCTT"))
-print(c.talk("COMP AAAAAAA"))
-print(c.talk("REV AGTCCC"))
+print(c.talk("GET 3"))
+print(c.talk("GET 4"))
+
+print("Testing INFO...")
+print(c.talk(f"INFO {seq0}"))
+
+print("Testing COMP...")
+print(c.talk(f"COMP {seq0}"))
+
+print("Testing REV...")
+print(c.talk(f"REV {seq0}"))
+
+print("Testing GENE...")
 print(c.talk("GENE U5"))
+print(c.talk("GENE ADA"))
+print(c.talk("GENE FRAT1"))
+print(c.talk("GENE FXN"))
+print(c.talk("GENE RNU6_269P"))
---
+++ P4/EX6.py
@@ -0,0 +1,107 @@
+import socket
+import termcolor
+from pathlib import Path
+
+# -- Server network parameters
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def read_fasta(filename):
+    file_contents = Path(filename).read_text().split("\n")[1:]
+    body = "".join(file_contents)
+    return body
+
+def process_client(s):
+    # -- Receive the request message
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    # -- Split the request messages into lines
+    lines = req.split('\n')
+
+    # -- The request line is the first
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    # -- Generate the response message
+    # It has the following lines
+    # Status line
+    # header
+    # blank line
+    # Body (content to send)
+
+    FOLDER = "../P4/"
+    file_request = req_line.split()[1]
+    # file_request is like req_line (GET /info/A HTTP/1.1) only with (/info/A) or  (/info/C)
+
+    if "/info/A" in file_request:
+        FILENAME = "A.html"
+        body = read_fasta(FOLDER + FILENAME)  # read() is the function read_fasta_data() from other practice
+    elif "/info/C" in file_request:
+        FILENAME = "C.html"
+        body = read_fasta(FOLDER + FILENAME)
+    elif "/info/G" in file_request:
+        FILENAME = "G.html"
+        body = read_fasta(FOLDER + FILENAME)
+    elif "/info/T" in file_request:
+        FILENAME = "T.html"
+        body = read_fasta(FOLDER + FILENAME)
+    elif file_request == "/":
+        FILENAME= "index.html"
+        body = read_fasta(FOLDER + FILENAME)
+    else:
+        FILENAME = "error.html"
+        body = read_fasta(FOLDER + FILENAME)
+
+    # This new contents are written in HTML language
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Setup up the socket's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Become a listening socket
+ls.listen()
+
+print("SEQ Server configured!")
+
+# --- MAIN LOOP
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        ls.close()
+        exit()
+    else:
+
+        # Service the client
+        process_client(cs)
+
+        # -- Close the socket
+        cs.close()
