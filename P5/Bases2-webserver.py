cdbdfb80c5232d17a0f991a6825d91e40f12678a
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-03-21 19:14:13 +0100

    Practise 5 finished

---
+++ P5/Bases2-webserver.py
@@ -0,0 +1,88 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+def read_fasta(filename):
+    file_contents = Path(filename).read_text().split("\n")[1:]
+    body = "".join(file_contents)
+    return body
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        # Message to send back to the clinet
+
+        request = self.requestline.split()[1]
+        folder = "../P5/"
+        if request == "/" or request == "/index.html":
+            FILENAME = "index.html"
+            contents = read_fasta(folder + FILENAME)
+            self.send_response(200)
+        else:
+            try:
+                file = request.split(".")[0]
+                FILENAME = file + ".html"
+                contents = read_fasta(folder + FILENAME)
+                self.send_response(200)
+            except FileNotFoundError:
+                FILENAME = "error.html"
+                contents = read_fasta(folder + FILENAME)
+                self.send_response(404)
+
+
+
+
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
