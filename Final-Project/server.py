eb961a5f5d7c868742fb2b919671099cb7e7aab6
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-04-29 19:45:02 +0200

    Final project session 2

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -3,24 +3,34 @@ import socketserver
 import termcolor
 from pathlib import Path
 from Seq1 import Seq
+import http.client
+import json
 
-# Define the Server's port
+def get_data(endpoint):
+    PORT = 8080
+    SERVER = 'rest.ensembl.org'
+    print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+    conn = http.client.HTTPConnection(SERVER, timeout=100)
+    try:
+        conn.request("GET", endpoint)
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+    r1 = conn.getresponse()
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+    data1 = r1.read().decode("utf-8")
+    response = json.loads(data1)
+    return response
+
+
 PORT = 8080
 
-sequence_list = ["ACT\n", "TTT\n", "AAA\n", "CCC\n", "GGG\n"]
-# -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 
-
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """This method is called whenever the client invokes the GET method
-        in the HTTP protocol request"""
 
-        # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
         path = self.requestline.split()[1]
@@ -31,6 +41,66 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = Path("main-page.html").read_text()
             error_code = 200
 
+        elif firts_argument == "/listSpecies":
+            error_code = 200
+            second_argument = arguments[1]
+            number = second_argument.split("=")[1]
+            data = get_data("info/species?content-type=application/json")
+            species = data["species"]
+            if number == "":
+                contents = f"""<!DOCTYPE html>
+                               <html lang="en">
+                               <head>
+                                   <meta charset="UTF-8">
+                                   <title>SPECIES</title>
+                               </head>
+                               <body style="background-color: lightyellow;">
+                                <p>Total number of species in the ensembl is: 267</p>
+                                <p>The limit you have selected is:{number}</p>
+                                <p>The names of the species are:</p>
+                               </body>
+                               </html>"""
+                for element in species:
+                    contents += f"""<p> - {element["common_name"]}</p> """
+
+            elif 267 >= int(number):
+                contents=f"""<!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>SPECIES</title>
+                            </head>
+                            <body style="background-color: lightyellow;">
+                                <p>Total number of species in the ensembl is: 267</p>
+                                <p>The limit you have selected is:{number}</p>
+                                <p>The names of the species are:</p>
+                            </body>
+                            </html>"""
+                counter = 0
+                for element in species:
+                    if counter < int(number):
+                        contents += f"""<p> - {element["common_name"]}</p> """
+                    counter += 1
+
+            else:
+                contents = f"""<!DOCTYPE html>
+                                   <html lang="en">
+                                   <head>
+                                       <meta charset="UTF-8">
+                                       <title>SPECIES</title>
+                                   </head>
+                                   <body style="background-color: lightyellow;">
+                                    <p>Total number of species in the ensembl is: 267</p>
+                                    <p>The limit you have selected is:{number}</p>
+                                    <p>The names of the species are:</p>
+                                   </body>
+                                   </html>"""
+                for element in species:
+                    contents += f"""<p> - {element["common_name"]}</p> """
+
+
+
+        #elif firts_argument == "/karyotype":
         else:
             contents = Path('Error.html').read_text()
             error_code = 404
