2092ac955f36f8e362432e94ec575b86b491043c
vanguix <v.anguix.2019@alumnos.urjc.es>
2020-05-24 12:22:40 +0200

    Final project session 7

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -37,6 +37,22 @@ def html_format(colour, title):
            </body>
            </html>"""
 
+def dict_listSpecies(limit, list):
+    contents = {
+        "Total number of species": 267,
+        "Limit selected": limit,
+        "List of species": list
+    }
+    client_dict = json.dumps(contents)
+    return client_dict
+
+def dict_karyotype(list):
+    contents = {
+        "The names of the chromosomes are": list
+    }
+    client_dict = json.dumps(contents)
+    return client_dict
+
 
 PORT = 8080
 
@@ -63,37 +79,37 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 error_code = 200
                 second_argument = arguments[1]
                 parameters = second_argument.split("&")
+                data = get_data("info/species?content-type=application/json")
                 if len(parameters) == 2:
                     parameter_1, json = second_argument.split("&")
                     number = parameter_1.split("=")[1]
-                    data = get_data("info/species?content-type=application/json")
 
                     if json == "json=1":
-                        contents = data["species"]  # Falta añadir el parámetro limit igual que arriba pero ahora en json
+                        list = []
+                        counter = 0
+                        if number == "":
+                            for element in data:
+                                list.append(element["display_name"])
+                                counter += 1
+                            contents = dict_listSpecies(number, list)
+                        elif 267 >= int(number):
+                            for element in data:
+                                if counter < int(number):
+                                        list.append(element["display_name"])
+                                        counter += 1
+                                contents = dict_listSpecies(number, list)
+                        else:
+                            for element in data:
+                                list.append(element["display_name"])
+                                counter += 1
+                            contents = dict_listSpecies(number, list)
 
                     else:
-                        species = data["species"]
-                        contents = html_format("lightyellow","List of species")
-                        contents += f"""<h1> SPECIES ON DATABASE OF ENSEMBL </h1>"""
-                        contents += f"""<p>Total number of species in the ensembl database is 267</p>"""
-                        contents += f"""<p>The limit you have selected is: {number}</p>"""
-                        contents += f"""<p>The names of the species are:</p>"""
-                        if number == "":
-                            for element in species:
-                                contents += f"""<p> - {element["display_name"]}</p> """
-                        elif 267 >= int(number):
-                            counter = 0
-                            for element in species:
-                                if counter < int(number):
-                                    contents += f"""<p> - {element["display_name"]}</p> """
-                                counter += 1
-                        else:
-                            for element in species:
-                                contents += f"""<p> - {element["display_name"]}</p> """
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(parameters) == 1:
                     number = second_argument.split("=")[1]
-                    data = get_data("info/species?content-type=application/json")
                     species = data["species"]
                     contents = html_format("lightyellow", "List of species")
                     contents += f"""<h1> SPECIES ON DATABASE OF ENSEMBL </h1>"""
@@ -126,12 +142,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         contents = data["karyotype"]
 
                     else:
-                        karyotype = data["karyotype"]
-                        contents = html_format("lightgreen", "Karyotype")
-                        contents += f"""<h1> KARYOTYPE </h1>"""
-                        contents += f"""<p>The names of the chromosomes from {specie} are:</p>"""
-                        for chromosome in karyotype:
-                            contents += f"""<p> - {chromosome}</p>"""
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(parameters) == 1:
                     specie = second_argument.split("=")[1]
@@ -150,7 +162,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 if len(arguments) == 3:
                     specie, chromo, json = second_argument.split("&")
                     specie_name = specie.split("=")[1]
-                    chromo_name = chromo.split("=")[1]
+                    #chromo_name = chromo.split("=")[1]
                     data = get_data(f"/info/assembly/{specie_name}?content-type=application/json")
 
                     if json == "json=1":
@@ -158,14 +170,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         #no se si aqui faltaría algo
 
                     else:
-                        information = data["top_level_region"]
-                        contents = html_format("salmon","Chromosome length")
-                        contents += "<h1> CHROMOSOME LENGTH </h1>"
-                        for chromosome in information:
-                            if chromosome["coord_system"] == "chromosome":
-                                if chromosome["name"] == chromo_name:
-                                    c_lenght = chromosome['length']
-                                    contents += f"<p>The length of the chromosome {chromo_name} is: {c_lenght}</p>"
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(arguments) == 2:
                     specie, chromo = second_argument.split("&")
@@ -193,10 +199,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     if json == "json=1":
                         contents = data
                     else:
-                        contents = html_format("pink", "Sequences")
-                        contents += "<h1> SEQUENCES OF HUMAN GENES </h1>"
-                        contents += f'<p> The sequence of gene {gene} is: </p>'
-                        contents += f'<textarea rows = "100" "cols = 500"> {data["seq"]} </textarea>'
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
                 elif len(parameters) == 1:
                     gene = second_argument.split("=")[1]
                     gene_id = get_data(f"""/xrefs/symbol/homo_sapiens/{gene}?content-type=application/json""")[0]["id"]
@@ -218,13 +222,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     if json == "json=1":
                         contents = data
                     else:
-                        contents = html_format("plum","Information")
-                        contents += f'<h1>INFORMATION ABOUT HUMAN GENE {gene} </h1>'
-                        contents += f'<p> The start point is: {data["start"]}</p>'
-                        contents += f'<p> The end point is: {data["end"]}</p>'
-                        contents += f'<p> The length of the gene is: {data["end"] - data["start"]}</p>'
-                        contents += f'<p> The id of the gene is: {gene_id}</p>'
-                        contents += f'<p> The gene is on chromosome {data["seq_region_name"]}</p>'
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(parameters) == 1:
                     gene = second_argument.split("=")[1]
@@ -250,13 +249,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     if json == "json=1":
                         contents = data
                     else:
-                        sequence = Seq(data["seq"])
-                        contents = html_format("lightblue", "Calculations")
-                        contents += F"<h1> CALCULATIONS OF GENE {gene}</h1>"
-                        contents += f"<p> Total length: {sequence.len()} </p>"
-                        contents += "<p> Percentage of its bases: </p>"
-                        for base in list_bases:
-                            contents += f"<p>- {base}: {sequence.seq_count_base(base)[1]}%</p>"
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(parameters) == 1:
                     gene = second_argument.split("=")[1]
@@ -283,11 +277,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     if json == "json=1":
                         contents = data
                     else:
-                        contents = html_format("peachpuff", "List of genes")
-                        contents += "<h1> LIST OF GENES </h1>"
-                        contents += f"<p> Here is the list of some genes from chromosome {chromo_name}:</p>"
-                        for gene in data:
-                            contents += f'<p> - {gene["external_name"]}</p>'
+                        contents = Path('Error.html').read_text()
+                        error_code = 404
 
                 elif len(parameters) == 3:
                     chromo, start, end = second_argument.split("&")
@@ -305,8 +296,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = Path('Error.html').read_text()
             error_code = 404
 
+        endpoints = ["/", "/listSpecies", "/karyotype", "/chromosomeLength",
+                     "/geneSeq", "/geneInfo", "/geneCalc", "/geneList"]
+
+        if firts_argument in endpoints:
+            if json == "json=1":
+                type = "application/json"
+            else:
+                type = "text/html"
+
+        self.send_header('Content-Type', type)
         self.send_response(error_code)
-        self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
         self.end_headers()
         self.wfile.write(str.encode(contents))
